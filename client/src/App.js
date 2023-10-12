6e8f659b959fec98f9b912b14584c68c73c2ee80
ErickSolisR16 <erick.sr.1601@gmail.com>
2023-10-12 17:14:28 -0600

    Creation of the task component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Task from './components/Task';
 
 function App() {
   return (
     <div className="App">
-
+      <div className='task-list'>
+        <h1>Mis Tareas</h1>
+        <Task text='Prueba 1'/>
+      </div>
     </div>
   );
 }
