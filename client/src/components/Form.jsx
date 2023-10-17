6c76f07a970cedaabfe090ef33e49bcb81de9327
ErickSolisR16 <erick.sr.1601@gmail.com>
2023-10-16 18:34:40 -0600

    Skeleton completion - remove tasks from the application

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import '../css/Form.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function Form(props) {
+
+  const [input, setInput] = useState();
+
+  const handleChange = event => {
+    setInput(event.target.value);
+  };
+  
+  const handleShipping = event => {
+    event.preventDefault();
+    const newTask = {
+      id: uuidv4(),
+      text: input,
+      completed: false
+    };
+    props.onSubmit(newTask);
+  };
+
   return (
-    <form className='task-form'>
+    <form 
+    className='task-form' 
+    onSubmit={handleShipping}>
       <input 
       className='input-task' 
       type='text'
       placeholder='Escribe una nueva tarea'
-      name='text' />
+      name='text'
+      onChange={handleChange} />
       <button className='button-task'>
         Agregar tarea
       </button>
--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from './Form';
+import Task from './Task';
 import '../css/TaskList.css';
 
 function TaskList() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = task => {
+    if (task.text.trim()) {
+      task.text = task.text.trim();
+      const updatedTasks = [task, ...tasks];
+      setTasks(updatedTasks);
+    }
+  };
+
+  const deleteTask = id => {
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const completedTask = id => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        task.completed = !task.completed;
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
   return (
     <>
-      <Form />
+      <Form onSubmit={addTask} />
       <div className='task-list-container'>
-        LISTA DE TAREAS
+        {
+          tasks.map((task) => 
+            <Task 
+              key={task.id}
+              id={task.id}
+              text={task.text}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completedTask={completedTask}
+            />
+          )
+        }
       </div>
     </>
   );
