91b66a7147abc67ed61c2e072909fec351515017
ErickSolisR16 <erick.sr.1601@gmail.com>
2023-10-18 00:58:24 -0600

    Backend CORS configuration

--- server/src/main/java/solis/erick/Views/Application.java
+++ server/src/main/java/solis/erick/Views/Application.java
@@ -2,8 +2,10 @@ package solis.erick.Views;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "solis.erick")
 public class Application {
 
 	public static void main(String[] args) {
